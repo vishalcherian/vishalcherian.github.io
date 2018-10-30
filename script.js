eabc72a868d88b28beac8487a971b79b9a2736da
vcherian2015 <vishal.cherian@uconn.edu>
2018-10-30 17:16:47 -0400

    centered content on mobile view, added js for logo change on dark background, etc.

--- script.js
+++ script.js
@@ -13,8 +13,59 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+var projects = document.getElementById("projects-screen");
+var lightLogo = document.getElementById("light-screen");
+var darkLogo = document.getElementById("dark-screen");
+var index = document.querySelector(".header.index");
+var indexMobile = document.querySelector(".header.index-mobile");
+var change = false;
+var stickPoint = getDistance();
+
+function getDistance() {
+  var topDist = projects.offsetTop;
+  return topDist;
+}
+
+window.onscroll = function(e) {
+    var distance = getDistance() - window.pageYOffset;
+    var offset = window.pageYOffset;
+    
+    if ( (distance <= 20) && !change) {
+        //if current logo and index are white background version
+        //change logo to white version with same characteristics
+        lightLogo.style.display = 'none';
+        darkLogo.style.display = 'block';
+        //change index to inverted version
+        change = true;
+    } else if (change && (offset <= stickPoint)){
+        //if current logo/index are black background version
+        //change logo back to black version
+        darkLogo.style.display = 'none';
+        lightLogo.style.display = 'block';
+        //change index to inverted version
+        change = false;
+    }
+}
+
+
+
 // all custom jQuery will go here
 $(document).ready(function() {
+    
+//    $(document).scroll(function() {
+//        var scrollTop = $('#projects-screen').scrollTop();
+//        var elementOffset = $('#projects-screen').offset().top;
+//        var distance = elementOffset - scrollTop;
+//        
+//        if (distance <= 0) {
+//            $('#lightLogo').fadeOut();
+//            $('#darkLogo').fadeIn();
+//        } else {
+//            $('#darkLogo').fadeOut();
+//            $('#lightLogo').fadeIn();
+//        }
+//    });
+    
     $(".index").click(function() {    
         $(".intro-header").fadeOut(200);
         $(".intro-footer").fadeOut(200);
